4b355e31a622e4fb32c67c4d1a5230f1626e230d
Jeremiah Davis <jdavis@horizontalintegration.com>
2022-03-24 17:06:46 -0500

    Add gulp file and package.json

---
+++ resources/starterkit/gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp');
+const cssnano = require('gulp-cssnano');
+const autoprefixer = require('gulp-autoprefixer');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const mode = require('gulp-mode')();
+
+gulp.task('process-sass', () => {
+    return gulp.src('assets/scss/index.scss')
+        .pipe(mode.development(sourcemaps.init()))
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['> 1%']
+        }))
+        .pipe(cssnano())
+        .pipe(mode.development(sourcemaps.write()))
+        .pipe(gulp.dest('assets/css'));
+});
+
+gulp.task('default', () => {
+
+    gulp.watch(
+        ['assets/scss/*.scss','assets/scss/*/*.scss'],
+        { ignoreInitial: false },
+        gulp.series('process-sass')
+    );
+
+});
